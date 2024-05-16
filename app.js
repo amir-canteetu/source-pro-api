e8c6c4187da66916bb0334afd9b033987288e363
amir-canteetu <amircanteetu@gmail.com>
2024-05-16 11:18:54 -0600

    async

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // app.js
 
-const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const tenderRoutes = require('./routes/tenderRoutes'); // Import tender routes
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express           = require('express');
+const userRoutes        = require('./routes/userRoutes');
+const tenderRoutes      = require('./routes/tenderRoutes'); // Import tender routes
+const bodyParser        = require('body-parser');
+const cors              = require('cors');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app               = express();
+const PORT              = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
--- models/tenderModel.js
+++ models/tenderModel.js
@@ -1,40 +1,63 @@
-// models/tenderModel.js
-
 const pool = require('../config/dbConfig');
 
 class Tender {
-  static getAllTenders(callback) {
-    pool.query('SELECT * FROM tenders', (error, results) => {
-      if (error) throw error;
-      callback(results);
+  static getAllTenders() {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM tenders', (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
     });
   }
 
-  static getTenderById(tenderId, callback) {
-    pool.query('SELECT * FROM tenders WHERE tender_id = ?', tenderId, (error, results) => {
-      if (error) throw error;
-      callback(results[0]);
+  static getTenderById(tenderId) {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM tenders WHERE tender_id = ?', tenderId, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results[0]);
+        }
+      });
     });
   }
 
-  static createTender(newTender, callback) {
-    pool.query('INSERT INTO tenders SET ?', newTender, (error, results) => {
-      if (error) throw error;
-      callback(results.insertId);
+  static createTender(newTender) {
+    return new Promise((resolve, reject) => {
+      pool.query('INSERT INTO tenders SET ?', newTender, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.insertId);
+        }
+      });
     });
   }
 
-  static updateTender(tenderId, updatedTender, callback) {
-    pool.query('UPDATE tenders SET ? WHERE tender_id = ?', [updatedTender, tenderId], (error, results) => {
-      if (error) throw error;
-      callback(results.changedRows);
+  static updateTender(tenderId, updatedTender) {
+    return new Promise((resolve, reject) => {
+      pool.query('UPDATE tenders SET ? WHERE tender_id = ?', [updatedTender, tenderId], (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.changedRows);
+        }
+      });
     });
   }
 
-  static deleteTender(tenderId, callback) {
-    pool.query('DELETE FROM tenders WHERE tender_id = ?', tenderId, (error, results) => {
-      if (error) throw error;
-      callback(results.affectedRows);
+  static deleteTender(tenderId) {
+    return new Promise((resolve, reject) => {
+      pool.query('DELETE FROM tenders WHERE tender_id = ?', tenderId, (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results.affectedRows);
+        }
+      });
     });
   }
 }
