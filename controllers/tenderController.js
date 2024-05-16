6e8914eca986a4f36d85d35c404ca715a2480670
amir-canteetu <amircanteetu@gmail.com>
2024-05-16 11:39:45 -0600

    added validation to tenders

--- controllers/tenderController.js
+++ controllers/tenderController.js
@@ -1,4 +1,14 @@
+const { validationResult } = require('express-validator');
 const Tender = require('../models/tenderModel');
+
+// Validation and sanitization middleware functions
+const validateTender = [
+  // Example validations for 'createTender' endpoint
+  body('title').notEmpty().withMessage('Title is required').trim().escape(),
+  body('short_description').optional().isLength({ max: 255 }).withMessage('Short description must be less than 255 characters').trim().escape(),
+  body('deadline').optional().isISO8601().toDate().withMessage('Invalid deadline format'),
+   
+];
 
 exports.getAllTenders = async (req, res, next) => {
   try {
@@ -19,26 +29,44 @@ exports.getTenderById = async (req, res, next) => {
   }
 };
 
-exports.createTender = async (req, res, next) => {
-  const newTender = req.body;
-  try {
-    const insertId = await Tender.createTender(newTender);
-    res.status(201).json({ message: 'Tender created successfully', tenderId: insertId });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.createTender = [
+  validateTender, // Apply input validation middleware
+  async (req, res, next) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-exports.updateTender = async (req, res, next) => {
-  const tenderId = req.params.tenderId;
-  const updatedTender = req.body;
-  try {
-    const rowsAffected = await Tender.updateTender(tenderId, updatedTender);
-    res.json({ message: 'Tender updated successfully', rowsAffected });
-  } catch (error) {
-    next(error);
+    const newTender = req.body;
+    try {
+      const insertId = await Tender.createTender(newTender);
+      res.status(201).json({ message: 'Tender created successfully', tenderId: insertId });
+    } catch (error) {
+      next(error);
+    }
   }
-};
+];
+
+exports.updateTender = [
+  validateTender, // Apply input validation middleware
+  async (req, res, next) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const tenderId = req.params.tenderId;
+    const updatedTender = req.body;
+    try {
+      const rowsAffected = await Tender.updateTender(tenderId, updatedTender);
+      res.json({ message: 'Tender updated successfully', rowsAffected });
+    } catch (error) {
+      next(error);
+    }
+  }
+];
 
 exports.deleteTender = async (req, res, next) => {
   const tenderId = req.params.tenderId;
