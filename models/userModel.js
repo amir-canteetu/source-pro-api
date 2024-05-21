173155f5bf3a8029822a0a3f66de7e990c7042f2
amir-canteetu <amircanteetu@gmail.com>
2024-05-21 17:44:32 -0600

    fix

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ class User {
 
   static getUserById(userId) {
     return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM users WHERE user_id = ?';
+      const query = 'SELECT * FROM users WHERE id = ?';
       pool.query(query, [userId], (error, results) => {
         if (error) {
           reject(error);
@@ -43,7 +43,7 @@ class User {
 
   static updateUser(userId, updatedUser) {
     return new Promise((resolve, reject) => {
-      const query = 'UPDATE users SET ? WHERE user_id = ?';
+      const query = 'UPDATE users SET ? WHERE id = ?';
       pool.query(query, [updatedUser, userId], (error, results) => {
         if (error) {
           reject(error);
@@ -57,7 +57,7 @@ class User {
   static deleteUser(userId) {
     return new Promise((resolve, reject) => {
       // Implement deletion logic for the user
-      const deleteQuery = 'DELETE FROM users WHERE user_id = ?';
+      const deleteQuery = 'DELETE FROM users WHERE id = ?';
       pool.query(deleteQuery, [userId], (error, results) => {
         if (error) {
           reject(error);
