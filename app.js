1e59daa91bd8c7877fe998d428876c68af1eebc0
amir-canteetu <amircanteetu@gmail.com>
2024-05-24 05:44:46 -0600

    added code comments

--- app.js
+++ app.js
@@ -7,9 +7,37 @@ import tenderRoutes from './routes/tenderRoutes.js';
 import companyRoutes from './routes/companyRoutes.js';  
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from "helmet";
 
 const app               = express();
 const PORT              = process.env.PORT || 3000;
+
+
+
+/*
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
+
+app.use(limiter);
+*/
+
+/*
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://yourfrontenddomain.com', // Replace with your frontend domain
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
+*/
+
+app.use(helmet());
 
 // Middleware
 app.use(cors());
