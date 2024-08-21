fe151dc7e4a25266194f103d7c18db23e814f525
amir-canteetu <amircanteetu@gmail.com>
2024-08-21 16:52:22 -0600

    from body-parser to express.json()

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import authRoutes from './routes/authRoutes.js';
 import authenticateJWT from './middleware/authMiddleware.js';
 import tenderRoutes from './routes/tenderRoutes.js';  
 import companyRoutes from './routes/companyRoutes.js';  
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from "helmet";
 import morgan from 'morgan';
@@ -15,38 +14,12 @@ const PORT              = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
-/*
-import { rateLimit } from 'express-rate-limit'
-
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
-
-// Apply the rate limiting middleware to all requests.
-app.use(limiter)
-*/
-
-/*
-const cors = require('cors');
-
-const corsOptions = {
-  origin: 'https://yourfrontenddomain.com', // Replace with your frontend domain
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-};
-
-app.use(cors(corsOptions));
-*/
 
 app.use(helmet());
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/v1/api/users', (req, res, next) => {
   //this function will be called on this path only
